b9204aba369a06db4f22336e6ede2c4909ab317d
Balaji Sukumaran <balaji-sukumaran@outlook.com>
2020-10-29 14:30:51 +0530

    InitialCommit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context, ILogger<ValuesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
